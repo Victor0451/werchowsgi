a08b1eb77dbbd91b4725fd737782d2871eef7518
Longo Victor <victor.m.longo@gmail.com>
2024-04-04 19:22:27 -0300

    Reubicacion de botones en modal de notificaciones para modulo de campañas

--- components/campanas/ModalImpNotificaciones.js
+++ components/campanas/ModalImpNotificaciones.js
@@ -1,103 +1,80 @@
 import React from "react";
 import { Fragment, useState } from "react";
 import {
-    Button,
-    Dialog,
-    DialogHeader,
-    DialogBody,
-    DialogFooter,
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
 } from "@material-tailwind/react";
 
 import Notificacion from "./Notificacion";
 import ReactToPrint from "react-to-print";
-import {
-    PrinterIcon
-} from "@heroicons/react/24/outline";
+import { PrinterIcon } from "@heroicons/react/24/outline";
 
-export default function ModalImpNotificaciones({
-    casos,
-    usu,
-    f
-}) {
-    let componentRef = React.createRef();
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(!open);
+export default function ModalImpNotificaciones({ casos, usu, f }) {
+  let componentRef = React.createRef();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(!open);
 
+  return (
+    <Fragment>
+      {f === "T" ? (
+        <Button size="sm" onClick={handleOpen}>
+          Imp. Notificaciones
+        </Button>
+      ) : f === "U" ? (
+        <Button size="sm" color="yellow" onClick={handleOpen} className="ml-1">
+          <PrinterIcon strokeWidth={2} className="h-3 w-3" />
+        </Button>
+      ) : null}
 
-    return (
-        <Fragment>
-
-            {
-                f === "T" ? (
-                    <Button size="sm" onClick={handleOpen}>
-                        Imp. Notificaciones
-                    </Button>
-                ) :
-                    f === 'U' ? (
-                        <Button size="sm" color="yellow" onClick={handleOpen} className="ml-1">
-                            <PrinterIcon strokeWidth={2} className="h-3 w-3" />
-                        </Button>
-                    ) : null
-            }
-
-
-            <Dialog
-                open={open}
-                handler={handleOpen}
-                animate={{
-                    mount: { scale: 1, y: 0 },
-                    unmount: { scale: 0.9, y: -100 },
-                }}
-                size="xl"
-            >
-                <DialogHeader>Notificaciones</DialogHeader>
-                <DialogBody divider className="h-[45vw] overflow-scroll">
-
-                    {f === 'U' ? (
-                        <div id="todo" ref={(el) => (componentRef = el)}>
-                            <Notificacion caso={casos} userData={usu.usuario} />
-                        </div>
-
-                    ) : f === 'T' ?
-                        (
-                            <div id="todo" ref={(el) => (componentRef = el)}>
-                                {casos.map((caso) => (
-                                    <>
-                                        <Notificacion caso={caso} userData={usu.usuario} />
-                                    </>
-                                ))}
-                            </div>
-                        ) : null}
-
-
-                </DialogBody>
-                <DialogFooter>
-
-                    <Button
-                        variant="gradient"
-                        color="green"
-
-                    >
-                        <ReactToPrint
-                            trigger={() => (
-                                <a href="#" className="btn btn-primary">
-                                    imprimir{" "}
-                                </a>
-                            )}
-                            content={() => componentRef}
-                        />
-                    </Button>
-                    <Button
-                        variant="gradient"
-                        color="red"
-                        onClick={handleOpen}
-                        className="ml-1"
-                    >
-                        <span>Cerrar</span>
-                    </Button>
-
-                </DialogFooter>
-            </Dialog>
-        </Fragment>
-    );
+      <Dialog
+        open={open}
+        handler={handleOpen}
+        animate={{
+          mount: { scale: 1, y: 0 },
+          unmount: { scale: 0.9, y: -100 },
+        }}
+        size="xl"
+      >
+        <DialogHeader>Notificaciones</DialogHeader>
+        <DialogFooter>
+          <Button variant="gradient" color="green">
+            <ReactToPrint
+              trigger={() => (
+                <a href="#" className="btn btn-primary">
+                  imprimir{" "}
+                </a>
+              )}
+              content={() => componentRef}
+            />
+          </Button>
+          <Button
+            variant="gradient"
+            color="red"
+            onClick={handleOpen}
+            className="ml-1"
+          >
+            <span>Cerrar</span>
+          </Button>
+        </DialogFooter>
+        <DialogBody divider className="h-[45vw] overflow-scroll">
+          {f === "U" ? (
+            <div id="todo" ref={(el) => (componentRef = el)}>
+              <Notificacion caso={casos} userData={usu.usuario} />
+            </div>
+          ) : f === "T" ? (
+            <div id="todo" ref={(el) => (componentRef = el)}>
+              {casos.map((caso) => (
+                <>
+                  <Notificacion caso={caso} userData={usu.usuario} />
+                </>
+              ))}
+            </div>
+          ) : null}
+        </DialogBody>
+      </Dialog>
+    </Fragment>
+  );
 }
